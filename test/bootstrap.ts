13839b41485d409458fa21754a6d251bf3fd5726
Victoria Johns <victoria.johns@groupbyinc.com>
2017-10-16 17:00:05 -0400

    Inintial

---
+++ test/bootstrap.ts
@@ -0,0 +1,5 @@
+import { bootstrap } from '@storefront/testing';
+import * as chai from 'chai';
+
+bootstrap(chai, __dirname, ['../src/infinite-scroll/index.html',
+]);
---
+++ test/unit/_suite.ts
@@ -0,0 +1,4 @@
+import { Suite } from '@storefront/testing';
+import * as chai from 'chai';
+
+export default Suite.base(chai);

ee328a36171f14350aa691b2c2acef1a71a21193
Victoria Johns <vmichellej@gmail.com>
2018-01-19 11:21:09 -0500

    Fix circleci cache

--- src/list/index.ts
+++ src/list/index.ts
@@ -3,6 +3,5 @@ import { List } from '@storefront/structure';
 
 @tag('gb-infinite-list', require('./index.html'), require('./index.css'))
 class InfiniteList extends List {}
-// class InfiniteList {}
 
 export default InfiniteList;
